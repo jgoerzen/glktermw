842eb2a803ea23d254e5381cd0f078b57ecefda7
John Goerzen <jgoerzen@complete.org>
2019-10-12 15:55:38 -0500

    cleaning up

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(glktermw):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="glktermw" command="/usr/bin/glktermw"
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libglktermw 1.0.4 glktermw (>> 1.0.4-0), glktermw (<< 1.0.4-99)
